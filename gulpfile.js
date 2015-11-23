750aeceaa6e8dcce9862ae2908a364017270456f
Nick Arcuri <narcuri1@gmail.com>
2015-11-23 15:43:56 -0500

    Layout index page

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,8 @@ var gulp = require('gulp'),
 		source = require('vinyl-source-stream'),
 		buffer = require('vinyl-buffer'),
 		sourcemaps = require('gulp-sourcemaps'),
-		babelify = require('babelify');
+		babelify = require('babelify'),
+		browserSync = require('browser-sync').create();
 
 gulp.task('demo-babel', function() {
 	var dbBundler = browserify('src/demo/database.js');
@@ -31,7 +32,7 @@ gulp.task('demo-babel', function() {
 		.pipe(buffer())
 		.pipe(sourcemaps.init({ loadMaps: true }))
 		.pipe(uglify({ mangle: false }))
-		.pipe(sourcemaps.write())
+		.pipe(sourcemaps.write(''))
 		.pipe(gulp.dest('./static/scripts'));
 });
 
@@ -44,10 +45,17 @@ gulp.task('server-babel', function() {
 gulp.task('sass', function() {
 	return gulp.src('./sass/*.scss')
 		.pipe(sass())
-		.pipe(gulp.dest('./static/styles'));
+		.pipe(gulp.dest('./static/styles'))
+		.pipe(browserSync.stream());
 });
 
 gulp.task('watch', ['demo-babel', 'server-babel', 'sass'], function() {
+	browserSync.init({
+		proxy: 'localhost:8080',
+		open: false
+	});
+
+	gulp.watch('./*.html').on('change', browserSync.reload);
 	gulp.watch('./src/demo/*.js', ['demo-babel']);
 	gulp.watch('./src/server/server.js', ['server-babel']);
 	gulp.watch('./sass/*.scss', ['sass']);
